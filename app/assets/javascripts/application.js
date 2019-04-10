f676c9a8382403fa25160f932751bceb2a5087fb
Carmen Kerim <carmen.kerim01@stclairconnect.ca>
2019-04-09 21:15:34 -0400

    added hotjar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,17 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+document.addEventListener('turbolinks:load', function (event) {
+    (function (h, o, t, j, a, r) {
+        h.hj = h.hj || function () {
+            (h.hj.q = h.hj.q || []).push(arguments)
+        };
+        h._hjSettings = {hjid: 1275506, hjsv: 6};
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script');
+        r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+});
